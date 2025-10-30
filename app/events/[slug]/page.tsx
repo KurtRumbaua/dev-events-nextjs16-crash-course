aecf83666b4ef15d26313c907c332ca1e77925d3
Kurt Ian Rumbaua <kurtianrumbaua@1.local>
2025-10-30 22:22:52 +0800

    WIP: Still working on displaying the event details

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,6 +1,33 @@
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const EventDetailItem = ({
+  icon,
+  alt,
+  label,
+}: {
+  icon: string;
+  alt: string;
+  label: string;
+}) => (
+  <div className="flex-row-gap-2 items-center">
+    <Image src={icon} alt={alt} width={17} height={17} />
+    <p>{label}</p>
+  </div>
+);
+
+const EventAgenda = ({ agendaItems }: { agendaItems: string[] }) => (
+  <div className="agenda">
+    <h2>Agenda</h2>
+    <ul>
+      {agendaItems.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+    </ul>
+  </div>
+);
 
 const EventDetailsPage = async ({
   params,
@@ -9,14 +36,83 @@ const EventDetailsPage = async ({
 }) => {
   const { slug } = await params;
   const request = await fetch(`${BASE_URL}/api/events/${slug}`);
-  const { event } = await request.json();
+  const {
+    event: {
+      title,
+      description,
+      image,
+      overview,
+      date,
+      time,
+      location,
+      mode,
+      agenda,
+      audience,
+      tags,
+    },
+  } = await request.json();
 
-  if (!event) return notFound();
+  if (!description) return notFound();
 
   return (
     <section id="event">
       <div className="event">
-        <h1 className="header">Event Description</h1>
+        <h1 className="header">{title}</h1>
+        <p>{description}</p>
+      </div>
+
+      <div className="details">
+        {/* Left Side - Event Content */}
+        <div className="content">
+          <Image
+            src={image}
+            alt="Event Banner"
+            width={800}
+            height={800}
+            className="banner"
+          />
+          <section className="flex-col-gap-2">
+            <h2>Overview</h2>
+            <p>{overview}</p>
+          </section>
+
+          <section className="flex-col-gap-2">
+            <h2>Event Details</h2>
+
+            <EventDetailItem
+              icon="/icons/calendar.svg"
+              alt="calendar"
+              label={`Date: ${date}`}
+            />
+            <EventDetailItem
+              icon="/icons/clock.svg"
+              alt="calendar"
+              label={`Time: ${time}`}
+            />
+            <EventDetailItem
+              icon="/icons/pin.svg"
+              alt="calendar"
+              label={`Venue: ${location}`}
+            />
+            <EventDetailItem
+              icon="/icons/mode.svg"
+              alt="calendar"
+              label={`Mode: ${mode}`}
+            />
+            <EventDetailItem
+              icon="/icons/audience.svg"
+              alt="calendar"
+              label={`Audience: ${audience}`}
+            />
+          </section>
+
+          <EventAgenda agendaItems={agenda} />
+        </div>
+
+        {/* Right Side - Booking Form */}
+        <aside className="booking">
+          <p className="text-lg font-semibold">Book Event</p>
+        </aside>
       </div>
     </section>
   );
