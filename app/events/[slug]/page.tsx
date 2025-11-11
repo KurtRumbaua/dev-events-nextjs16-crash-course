d1b9737ab0b3041d3a1db535bf3da940fef55537
KurtRumbaua <kurtrtumbaua28@gmail.com>
2025-11-11 19:26:03 +0800

    finalize the app

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -2,6 +2,7 @@ import BookEvent from "@/components/BookEvent";
 import EventCard from "@/components/EventCard";
 import { IEvent } from "@/database";
 import { getSimilarEventsBySlug } from "@/lib/actions/event.actions";
+import { cacheLife } from "next/cache";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
@@ -48,6 +49,8 @@ const EventDetailsPage = async ({
 }: {
   params: Promise<{ slug: string }>;
 }) => {
+  "use cache";
+  cacheLife("hours");
   const { slug } = await params;
   const request = await fetch(`${BASE_URL}/api/events/${slug}`);
   if (!request.ok) {
@@ -81,9 +84,13 @@ const EventDetailsPage = async ({
 
   if (!description) return notFound();
 
+  const event = payload.event;
+
   const bookings = 10;
 
-  const similarEvents: IEvent[] = await getSimilarEventsBySlug(slug);
+  const similarEvents = (await getSimilarEventsBySlug(
+    slug
+  )) as unknown as IEvent[];
 
   console.log({ similarEvents });
   return (
@@ -160,7 +167,7 @@ const EventDetailsPage = async ({
               <p className="text-sm">Be the first to book your spot!</p>
             )}
 
-            <BookEvent />
+            <BookEvent eventId={event._id} slug={event.slug} />
           </div>
         </aside>
       </div>
